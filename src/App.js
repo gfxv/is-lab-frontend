98485be9af68488d6bf4a8d624fe477de2efa98c
gfxv <superfreezu@gmail.com>
2024-10-16 16:38:01 +0300

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
+import DashboardPage from "./pages/Dashboard";
+
+const isAuthenticated = () => {
+  return localStorage.getItem("token") !== null;
+};
+
+const PrivateRoute = ({ children }) => {
+  return isAuthenticated() ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/"
+          element={<PrivateRoute>{<Navigate to="/dashboard" />}</PrivateRoute>}
+        />
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute>{<DashboardPage />}</PrivateRoute>}
+        />
+      </Routes>
+    </Router>
   );
 }
 

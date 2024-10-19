35717fd8db1609155bacf2bd83dfad609f907105
gfxv <superfreezu@gmail.com>
2024-10-19 14:58:29 +0300

    feat: basic dashboard layout

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -30,34 +30,51 @@ const Dashboard = () => {
         }
     };
 
-    return (
-        <div className="home-page">
-            {/* <div className="user-info">
-                <span>Welcome, {user.username}</span>
-                <button onClick={handleSendModeratorRequest}>Request Moderator</button>
-                <button onClick={onLogout}>Logout</button>
-            </div> */}
-            <h1>Objects Table</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {objects.map(object => (
-                        <tr key={object.id}>
-                            <td>{object.id}</td>
-                            <td>{object.name}</td>
-                            <td>{object.description}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className="max-w-5xl m-auto overflow-x-auto">
+      <h1 className="text-2xl font-bold mt-6 mb-1">Space Marine Dashboard</h1>
+      <div className="flex justify-end">
+        <Link
+          class="rounded-md bg-sky-600 py-2 px-3 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-sky-700 focus:shadow-none active:bg-sky-700 hover:bg-sky-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          to={"/marines/new"}
+        >
+          New Space Marine
+        </Link>
+      </div>
+      <table className="min-w-full mt-1 bg-white border border-gray-300 rounded-lg shadow-md">
+        <thead>
+          <tr className="bg-gray-100 text-gray-600 uppercase text-sm">
+            <th className={thStyles}>ID</th>
+            <th className={thStyles}>Name</th>
+            <th className={thStyles}>Creation Date</th>
+            <th className={thStyles}>Health</th>
+            <th className={thStyles}>Weapon</th>
+            <th className={thStyles}>Melee Weapon</th>
+            <th className={thStyles}>Coordinate</th>
+            <th className={thStyles}>Chapter ID</th>
+            <th className={thStyles}>Owner</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id} className="hover:bg-gray-50 hover:cursor-pointer">
+              <td className={tdStyles}>{item.id}</td>
+              <td className={tdStyles}>{item.name}</td>
+              <td className={tdStyles}>{item.creationDate}</td>
+              <td className={tdStyles}>{item.health}</td>
+              <td className={tdStyles}>{item.weapon}</td>
+              <td className={tdStyles}>{item.meleeWeapon}</td>
+              <td className={tdStyles}>
+                ({item.coordinates.x}; {item.coordinates.y})
+              </td>
+              <td className={tdStyles}>{item.chapter.id}</td>
+              <td className={tdStyles}>{item.owner.username}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Dashboard;

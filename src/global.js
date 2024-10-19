f2ccf9fb73c365773878be0d09977697b72ab530
gfxv <superfreezu@gmail.com>
2024-10-19 17:57:05 +0300

    chore

--- src/global.js
+++ src/global.js
@@ -1,5 +1,7 @@
 const baseUrl = "http://localhost:8080/api";
+const wsBaseUrl = "http://localhost:8080/ws";
 
 const getBaseUrl = () => baseUrl;
+const getWsBaseUrl = () => wsBaseUrl;
 
-export { getBaseUrl };
+export { getBaseUrl, getWsBaseUrl };

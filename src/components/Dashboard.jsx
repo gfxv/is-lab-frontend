0eefe9bedd4d1ab6f3e615e01d91b8d60cbaded1
gfxv <superfreezu@gmail.com>
2024-10-19 14:59:19 +0300

    feat: add websocket suction

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,34 +1,87 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from "react";
+import SockJS from "sockjs-client";
+import { Stomp } from "@stomp/stompjs";
+import { getBaseUrl, getWsBaseUrl } from "../global";
+import { Link } from "react-router-dom";
 
+import axios from "axios";
 
 const Dashboard = () => {
-    const homeUrl = "localhost:8080/api/home";
-    const adminUrl = "localhost:8080/api/admin/request";
+  const [data, setData] = useState([]);
+  const wsUrl = getWsBaseUrl();
+  const stompClientRef = useRef(null);
 
-    const [objects, setObjects] = useState([]);
-    
-    useEffect(() => {
-        fetchObjects();
-    }, []);
+  const getRecords = () => {
+    axios
+      .get(getBaseUrl() + "/marines", {
+        params: {
+          page: 0,
+          size: 5,
+        },
+      })
+      .then((response) => {
+        updateTable(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
-    const fetchObjects = async () => {
-        try {
-            const response = await fetch(`${homeUrl}/marines`);
-            const data = await response.json();
-            setObjects(data);
-        } catch (error) {
-            console.error('Error fetching objects:', error);
-        }
+  useEffect(() => {
+    stompClientRef.current = Stomp.over(() => new SockJS(wsUrl));
+
+    stompClientRef.current.connect({}, (frame) => {
+      console.log("Connected to WebSocket: " + frame);
+      stompClientRef.current.subscribe("/records/changes", (message) => {
+        console.log("[ws.subscribe]", message.body);
+        updateTable(JSON.parse(message.body));
+      });
+
+      console.log("calling getRecords()");
+      getRecords();
+    });
+
+    stompClientRef.current.debug = (str) => {
+      console.log("WS Debug:", str);
     };
 
-    const handleSendModeratorRequest = async () => {
-        try {
-            await fetch(adminUrl, { method: 'POST' });
-            alert('Moderator request sent successfully!');
-        } catch (error) {
-            console.error('Error sending moderator request:', error);
-        }
+    stompClientRef.current.activate();
+    return () => {
+      if (stompClientRef.current) {
+        stompClientRef.current.disconnect(() => {
+          console.log("Disconnected");
+        });
+      }
     };
+  }, []);
+
+  const updateTable = (message) => {
+    switch (message.type) {
+      case "GET":
+        setData(message.records);
+        break;
+      case "ADD":
+        setData((prevData) => [...prevData, message.records]);
+        break;
+      case "UPDATE":
+        setData((prevData) =>
+          prevData.map((item) =>
+            item.id === message.records[0].id ? message.records[0] : item
+          )
+        );
+        break;
+      case "DELETE":
+        setData((prevData) =>
+          prevData.filter((item) => item.id !== message.records[0].id)
+        );
+        break;
+      default:
+        break;
+    }
+  };
+
+  const thStyles = "py-1 px-1 border-b";
+  const tdStyles = "py-2 px-2 border-b text-center";
 
   return (
     <div className="max-w-5xl m-auto overflow-x-auto">

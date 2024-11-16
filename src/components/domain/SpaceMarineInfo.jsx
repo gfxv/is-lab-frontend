7814297555b43b1c667fd747d6c867bbdae07df1
gfxv <superfreezu@gmail.com>
2024-11-16 10:32:36 +0300

    feat: display delete button to users with permission

--- src/components/domain/SpaceMarineInfo.jsx
+++ src/components/domain/SpaceMarineInfo.jsx
@@ -1,14 +1,15 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { getBaseUrl } from "../../global";
-import { Link } from "react-router-dom";
-
 
 import NewCoordinatesModal from "./NewCoordinatesModal";
 import NewChapterModal from "./NewChapterModal";
 import CoordinatesTable from "./CoordinatesTable";
 import ChapterTable from "./ChapterTable";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 const SpaceMarineCard = ({ id }) => {
   const [availableWeapons, setAvailableWeapons] = useState([]);
@@ -38,6 +39,9 @@ const SpaceMarineCard = ({ id }) => {
   const [isCoordinateSelected, setIsCoordinateSelected] = useState(false);
   const [isChapterSelected, setIsChapterSelected] = useState(false);
 
+  const [canEdit, setCanEdit] = useState(false);
+  const [isEditMode, setIsEditMode] = useState(false);
+
   const token = localStorage.getItem("token");
   const config = {
     headers: {
@@ -46,6 +50,21 @@ const SpaceMarineCard = ({ id }) => {
     },
   };
   const navigate = useNavigate();
+
+  const handleDelete = () => {
+    axios
+      .delete(`${getBaseUrl()}/marines/${id}`)
+      .then((response) => {
+        if (response.status === 200) {
+          navigate("/")
+        } else {
+          console.error("Failed to delete item");
+        }
+      })
+      .catch((error) => {
+        console.error("Error occurred while deleting item:", error);
+      });
+  };
 
   const onCoordinateSelection = (newCoordinate) => {
     setCoordinates(newCoordinate);
@@ -57,15 +76,36 @@ const SpaceMarineCard = ({ id }) => {
     setIsChapterSelected(true);
   };
 
+  const modifyObject = () => {
+    // set inputs to 'modify' mode
+    // show table + 'create new' buttons
+    // ...
+  };
+
   useEffect(() => {
     axios
-    .get(getBaseUrl() + `/marines/${id}`)
-    .then((response) => {
-      setData(response.data);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+      .get(getBaseUrl() + `/marines/has-access/${id}`, {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        setCanEdit(response.data);
+        setIsEditMode(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+    axios
+      .get(getBaseUrl() + `/marines/${id}`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
 
     axios
       .get(getBaseUrl() + "/marines/weapons")
@@ -84,7 +124,6 @@ const SpaceMarineCard = ({ id }) => {
       .catch((error) => {
         console.error("Error:", error);
       });
-
   }, []);
 
   const setData = (info) => {
@@ -94,19 +133,23 @@ const SpaceMarineCard = ({ id }) => {
     setHeight(info.height);
     setWeapon(info.weapon);
     setMeleeWeapon(info.meleeWeapon);
-    setCoordinates({id: info.coordinates.id, x: info.coordinates.x, y: info.coordinates.y})
+    setCoordinates({
+      id: info.coordinates.id,
+      x: info.coordinates.x,
+      y: info.coordinates.y,
+    });
     setChapter({
       id: info.chapter.id,
       name: info.chapter.name,
       parentLegion: info.chapter.parentLegion,
       marinesCount: info.chapter.marinesCount,
       world: info.chapter.world,
-    })
-    setOwner(info.setOwner)
+    });
+    setOwner(info.setOwner);
 
-    setIsCoordinateSelected(true)
-    setIsChapterSelected(true)
-  }
+    setIsCoordinateSelected(true);
+    setIsChapterSelected(true);
+  };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -120,84 +163,94 @@ const SpaceMarineCard = ({ id }) => {
       weapon: weapon,
       meleeWeapon: meleeWeapon,
       coordinates: coordinates,
-      chapter: chapter
-    }
+      chapter: chapter,
+    };
 
-    console.log(updatedMarine)
+    console.log(updatedMarine);
 
     axios
       .put(`${getBaseUrl()}/marines`, updatedMarine, config)
       .then((response) => {
         console.log("Space Marine updated successfully: ", response.status);
-        navigate("/")
+        navigate("/");
       })
       .catch((error) => {
         console.error("Error updating user:", error);
       });
-  }
+  };
 
   return (
-    <div class="w-full max-w-lg m-auto mt-5">
-      <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4">
+    <div className="w-full max-w-lg m-auto mt-5 relative">
+      {canEdit && (
+        <div className="absolute top-1 right-1">
+          <button 
+          onClick={ handleDelete }
+          className=" bg-red-500 text-white px-2 py-1 rounded shadow-lg hover:bg-red-600">
+            <FontAwesomeIcon icon={faTrashCan} />
+          </button>
+        </div>
+      )}
+
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="username"
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
           >
             Name:
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             type="text"
             id="username"
             placeholder="Space Marine name"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            required
+            readOnly={!isEditMode}
           />
         </div>
 
-        <div class="mb-4">
+        <div className="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="username"
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
           >
             Health:
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             type="number"
             value={health}
             onChange={(e) => setHealth(e.target.value)}
             min="0"
-            required
+            readOnly={!isEditMode}
           />
         </div>
 
-        <div class="mb-4">
+        <div className="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="username"
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
           >
             Height:
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             type="number"
             value={height}
             onChange={(e) => setHeight(e.target.value)}
             min="0"
-            required
+            readOnly={!isEditMode}
           />
         </div>
 
         <div className="flex flex-col md:flex-row gap-4">
           <div className="flex-1 mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
               Weapon:
             </label>
             <select
-              class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
               value={weapon}
               onChange={(e) => setWeapon(e.target.value)}
               required
@@ -211,11 +264,11 @@ const SpaceMarineCard = ({ id }) => {
           </div>
 
           <div className="flex-1 mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
               Melee Weapon:
             </label>
             <select
-              class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
               value={meleeWeapon}
               onChange={(e) => setMeleeWeapon(e.target.value)}
               required
@@ -229,67 +282,73 @@ const SpaceMarineCard = ({ id }) => {
           </div>
         </div>
 
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
             Coordinates:
           </label>
           {isCoordinateSelected && (
             <div>
-              <label class="block text-gray-700 text-sm font-mono">
+              <label className="block text-gray-700 text-sm font-mono">
                 Selected Coordinate:
               </label>
-              <label class="block text-gray-700 text-sm font-mono">
-                x: {coordinates.x} 
+              <label className="block text-gray-700 text-sm font-mono">
+                x: {coordinates.x}
               </label>
-              <label class="block text-gray-700 text-sm font-mono mb-2">
-                y: {coordinates.y} 
+              <label className="block text-gray-700 text-sm font-mono mb-2">
+                y: {coordinates.y}
               </label>
             </div>
           )}
-          <label class="block text-gray-500 text-sm">Select from table</label>
-          <CoordinatesTable onRowClick={onCoordinateSelection}
-          />
-          <label class="block text-gray-500 text-sm mt-2">
+          <label className="block text-gray-500 text-sm">
+            Select from table
+          </label>
+          <CoordinatesTable onRowClick={onCoordinateSelection} />
+          <label className="block text-gray-500 text-sm mt-2">
             Or create a new one
           </label>
           <button
-            class="rounded-md bg-sky-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-sky-700 focus:shadow-none active:bg-sky-700 hover:bg-sky-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            className="rounded-md bg-sky-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-sky-700 focus:shadow-none active:bg-sky-700 hover:bg-sky-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button"
             onClick={() => setIsCoordinatesModalOpen(true)}
           >
             New coordinate
           </button>
         </div>
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
             Chapter:
           </label>
-          { isChapterSelected && (
+          {isChapterSelected && (
             <div>
-              <label class="block text-gray-700 text-sm font-mono">
+              <label className="block text-gray-700 text-sm font-mono">
                 Selected Chapter:
               </label>
-              <label class="block text-gray-700 text-sm font-mono">
-                name: {chapter.name} 
+              <label className="block text-gray-700 text-sm font-mono">
+                name: {chapter.name}
               </label>
-              <label class="block text-gray-700 text-sm font-mono">
-                parent legion: {chapter.parentLegion} 
+              <label className="block text-gray-700 text-sm font-mono">
+                parent legion: {chapter.parentLegion}
               </label>
-              <label class="block text-gray-700 text-sm font-mono">
-                marines count: {chapter.marinesCount} 
+              <label className="block text-gray-700 text-sm font-mono">
+                marines count: {chapter.marinesCount}
               </label>
-              <label class="block text-gray-700 text-sm font-mono mb-2">
-                world: {chapter.world} 
+              <label className="block text-gray-700 text-sm font-mono mb-2">
+                world: {chapter.world}
               </label>
             </div>
           )}
-          <label class="block text-gray-500 text-sm">Select from table</label>
-          <ChapterTable data={availableChapters} onRowClick={onChapterSelection} />
-          <label class="block text-gray-500 text-sm mt-2">
+          <label className="block text-gray-500 text-sm">
+            Select from table
+          </label>
+          <ChapterTable
+            data={availableChapters}
+            onRowClick={onChapterSelection}
+          />
+          <label className="block text-gray-500 text-sm mt-2">
             Or create a new one
           </label>
           <button
-            class="rounded-md bg-sky-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-sky-700 focus:shadow-none active:bg-sky-700 hover:bg-sky-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            className="rounded-md bg-sky-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-sky-700 focus:shadow-none active:bg-sky-700 hover:bg-sky-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button"
             onClick={() => setIsChapterModalOpen(true)}
           >
@@ -297,13 +356,12 @@ const SpaceMarineCard = ({ id }) => {
           </button>
         </div>
 
-        <div class="flex justify-center">
+        <div className="flex justify-center">
           <button
             onClick={handleFormSubmit}
-            class="rounded-md bg-green-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-700 focus:shadow-none active:bg-green-700 hover:bg-green-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+            className="rounded-md bg-green-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-700 focus:shadow-none active:bg-green-700 hover:bg-green-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
             type="submit"
           >
-            {/* TODO: add redirect to /dashboard */}
             Save changes
           </button>
         </div>

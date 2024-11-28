37245c0ec8f2d22d0b0f088eff51b280b59ea273
gfxv <superfreezu@gmail.com>
2024-11-28 17:36:25 +0300

    feat: add Aggregators page

---
+++ src/pages/Agregators.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { getBaseUrl } from "../global";
+import WeaponTypeGroup from "../components/WeaponTypeGroup";
+import HeightCount from "../components/HeightCount";
+import Header from "../components/Header";
+
+const Aggregators = () => {
+
+  return (
+    <>
+      <Header />
+      <div className="max-w-5xl m-auto overflow-x-auto">
+        <h1 className="text-2xl font-bold mt-6 mb-1">Aggregators Page</h1>
+        <div className="mb-4">
+          <HeightCount/>
+        </div>
+        <div className="mb-4">
+          <WeaponTypeGroup />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Aggregators;
